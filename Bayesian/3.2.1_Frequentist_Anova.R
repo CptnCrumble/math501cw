2ee5d76591a79412e5d3f0a264fa4f0ba1649342
CptnCrumble <paul_hazell@live.co.uk>
2020-03-25 16:10:46 +0000

    3.2.1A-D first draft

--- Bayesian/3.2.1_Frequentist_Anova.R
+++ Bayesian/3.2.1_Frequentist_Anova.R
@@ -13,7 +13,7 @@ df_wide <-  cbind(data.frame(rbind(f_one,f_two,f_three,f_four)),c("f_one","f_two
 colnames(df_wide) <- c("X1","X2","X3","X4","X5","Fertilizer")
 # !!!- CHOP ??? ---------------------------------------------------------------------------------------
 
-df_long <- read.csv('./yield_data.csv')
+df_long <- read.csv('./yield_data.csv',header = FALSE)
 colnames(df_long) <- c("Fertilizer","Yield","plot_size")
 df_long$pch <- factor(df_long$plot_size)
 
@@ -21,24 +21,36 @@ df_long$pch <- factor(df_long$plot_size)
 # Box-plot
 ggplot(data=df_long, aes(x=Fertilizer, y=Yield))+
   geom_point(data=df_long,pch=21,alpha=0.5,aes(bg=Fertilizer, size=plot_size))+
-  scale_size_continuous(range = c(7, 10))+
+  scale_size_continuous(range = c(10,8))+
   ggtitle("Crop yield grouped by fertilizer used")+
   xlab("Fertilizer used")+
   ylab("Yield (ton/hectare)")+
   theme(legend.position = "none",
         plot.title =element_text(hjust = 0.5))+
   scale_fill_brewer(palette="Spectral")
-
-
-# Scatter plot - colour co-ordinated
+# + in Mean values
+# + more values on the y axis
 
 # Part B - Explain what alpha is:
 # alphj is the difference between the mean yield of the fields exposed to fertilizer 1 and the mean yield of the fields exposed to fertilizer j.
 # This allows us to reframe our hypothesis such that if no their is np difference between the fertilizers then
 # a2=a3=a4=a5=0
 
-# Part c -
-df_long %>% group_by(Fertilizer) %>% summarise(mean_yeild = mean(Yield),
-                                               mean_sd = sd(Yield))
+# Part C - Fit the model in Frequentist framework, Report means & alphas, Perform Freq. Hypothsis test, report conclusion with justification
+## Fit the model
+yield_lm <- lm(Yield ~ Fertilizer,data = df_long)
+## Report Means and Alphas
+alphas <- yield_lm$coefficients
+names(alphas) <- c("mu_hat","alpha2","alpha3","alpha4")
+alphas
+## Frequentist Hypothesis test
+### ANOVA test for rejecting null hypothesis of all means being equivalent
+yield_MM <- model.matrix(yield_lm)
+### "If we take a SIZE of 0.05 to be our threshold then our P value of 0.03388 means we can reject our null hypothesis that the mean yields of each of the groups is equivalent.
 
-#Part D - Fit the model in the frequentist framework
+
+#Part D - Tukey
+
+
+
+# Part E - some question about Units???
