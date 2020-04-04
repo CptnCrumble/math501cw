460fc7bed50a65573479ad20dad0114ac6ab6948
CptnCrumble <paul_hazell@live.co.uk>
2020-04-04 20:46:14 +0100

    Simple Bayes model added

--- Bayesian/3.2.2_Bayesian_ANOVA.R
+++ Bayesian/3.2.2_Bayesian_ANOVA.R
@@ -1,6 +1,7 @@
 # Requires source from 3.2.1_Frequentist_Anova.R
 
 library(R2jags)
+library(ggmcmc)
 
 # PART F - WRITE BUGS CODE
 
@@ -34,14 +35,74 @@ Bayesian_anova <- function(){
   
   tau ~ dgamma(0.001,0.001)
   sigma <- 1.0 / sqrt(tau)
+  
+  mean_yield[1] = m + alpha[1]
+  mean_yield[2] = m + alpha[2]
+  mean_yield[3] = m + alpha[3]
+  mean_yield[4] = m + alpha[4]
 }
 # Run JAGS
 Bayesian_anova_inference <- jags(data = data_anova, 
                                  parameters.to.save = c("m", # m is mu above
                                                         "alpha", 
                                                         "sigma", 
-                                                        "tau"), 
+                                                        "tau",
+                                                        "mean_yield"), 
                                  n.iter = 100000, 
                                  n.chains = 3,
                                  model.file = Bayesian_anova)
 
+# PART G - Graph of Posterior densities
+
+mcmc_data <- as.mcmc(Bayesian_anova_inference)
+ggs_data <- ggs(mcmc_data)
+
+
+ggs_density(ggs_data, family = "^alpha")+xlim(-10,10)
+# + better titles all round
+# Need discussion points
+
+# PART H - plots of confidence intervals
+# Plot of means
+ggs_caterpillar(ggs_data,family = "^mean_yield")
+# Plot of alphas
+ggs_caterpillar(ggs_data,family = "^alpha")
+
+
+# PART I - chat
+print(Bayesian_anova_inference, intervals = c(0.025,0.5,0.975))
+# could use some qbinom??
+
+# PART J 
+# Hmmmmm, further reading required
+
+# Part K - the simpler model
+simple_bayes_model <- function(){
+  
+  for(k in 1:n){
+    y[k] ~ dnorm(mu[k], tau)
+    mu[k] <- fertilizer[group[k]]
+  }
+  
+  for(i in 1:I){ 
+    fertilizer[i] ~ dnorm(0.0,0.0001)
+  }
+  
+  tau ~ dgamma(0.001,0.001)
+  sigma <- 1.0 / sqrt(tau)
+  
+}
+
+# Simple JAGS
+simple_jags <- jags(data = data_anova, 
+                    parameters.to.save = c("fertilizer", 
+                                           "sigma", 
+                                           "tau"), 
+                    n.iter = 100000, 
+                    n.chains = 3,
+                    model.file = simple_bayes_model)
+
+print(simple_jags, intervals = c(0.025,0.5,0.975))
+ggs_simple <- ggs(as.mcmc(simple_jags))
+ggs_caterpillar(ggs_simple,family = "^fertilizer")
+# Not convinced...
